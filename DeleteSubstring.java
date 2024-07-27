219285d6401b6369c9245ae7a3c17d6a8e35cbe8
Tanushree227 <tanushreedas813@gmail.com>
2024-07-27 20:45:43 +0530

    java implementation to delete a substring from a text

---
+++ DeleteSubstring.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+
+    public class DeleteSubstring {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a String: ");
+            String str = sc.nextLine();
+            System.out.println("Enter a Substring to delete: ");
+            String sub = sc.nextLine();
+    
+            String result = deleteSubstring(str, sub);
+            System.out.println("Resulting Text: " + result);
+        }
+    
+        public static String deleteSubstring(String text, String substring) {
+            if (text == null || substring == null) {
+                throw new IllegalArgumentException("Text or substring cannot be null");
+            }
+    
+            return text.replace(substring, "");
+        }
+    }
