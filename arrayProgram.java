93bc09a46346737051e4df922bbfc6c9070555a9
Tanushree227 <tanushreedas813@gmail.com>
2024-08-01 21:52:47 +0530

    reverse of an array

--- arrayProgram.java
+++ arrayProgram.java
@@ -9,4 +9,11 @@ public class arrayProgram {
         return max;
     }
 
+    public static int[] reverseArr(int[] arr) {
+        int[] reversed = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            reversed[arr.length - 1 - i] = arr[i];
+        }
+        return reversed;
+    }
 }
