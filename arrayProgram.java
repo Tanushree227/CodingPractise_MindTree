f8feae396f30d66ad11e70582fbcc56a2a8e67b7
Tanushree227 <tanushreedas813@gmail.com>
2024-08-01 21:53:52 +0530

    finding the missing element in an array

--- arrayProgram.java
+++ arrayProgram.java
@@ -27,4 +27,14 @@ public class arrayProgram {
         }
         return -1; // No duplicates found
     }
+
+    public static int findMissing(int[] arr) {
+        int n = arr.length + 1;
+        int expectedSum = (n * (n + 1)) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            actualSum += arr[i];
+        }
+        return expectedSum - actualSum;
+    }
 }
